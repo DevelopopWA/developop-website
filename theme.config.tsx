34ef3c1952b149401eb6beb568e434b67a4806cf
Musa <malikmusa1323@gmail.com>
2023-04-05 00:58:25 -0400

    main: commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>DeveloPop</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Spherrrical/developop-website',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Spherrrical/developop-website',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Developed by Musa',
   },
 }
 
